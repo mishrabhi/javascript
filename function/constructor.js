5634881746dbcab3b900a8ba4deac83b21b8616f
unknown <asmyselfabhishek00@gmail.com>
2020-11-18 22:46:58 -0800

    added constructor function

---
+++ function/constructor.js
@@ -0,0 +1,21 @@
+//To create a constructor function, we use new keyword.
+
+var Car = function(maxSpeed, driver){
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
+        console.log(speed * time)
+    };
+    this.driverName = function(){
+        console.log("Driver name is" + this.driver)
+    };
+}
+var myCar = new Car(50, "Abhishek")
+var myCar2 = new Car(60, "Ashu")
+var myCar3 = new Car(45, "Ashutosh")
+var myCar4 = new Car(70, "Satyam")
+var myCar5 = new Car(80, "Shivam")
+
+myCar.drive(45, 6)
+myCar4.driverName()
+myCar5.drive(50,5)
