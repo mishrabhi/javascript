681115d5d702e45ed2ae8619de41444268847a9e
unknown <asmyselfabhishek00@gmail.com>
2020-12-03 21:59:06 -0800

    added callback challenges

--- function/callbackChallenges.js
+++ function/callbackChallenges.js
@@ -5,3 +5,11 @@ function addTwo(num) {
 }
 console.log(addTwo(5))
 console.log(addTwo(9))
+
+//Challenge 2:
+//Create a function addS that accepts one input and add 'S' to it.
+function addS(input) {
+    return input+ 's' 
+}
+console.log(addS('Abhishek'))
+console.log(addS('Apple'))
