0de490915602ff08e690b493d3b51fef7e4e8c4d
unknown <asmyselfabhishek00@gmail.com>
2020-12-03 22:29:36 -0800

    added callBack challenges

--- function/callbackChallenges.js
+++ function/callbackChallenges.js
@@ -13,3 +13,25 @@ function addS(input) {
 }
 console.log(addS('Abhishek'))
 console.log(addS('Apple'))
+
+//Challenge 3:
+//Create a function map that takes two inputs:
+//1.an array of numbers(a list of numbers)
+//2.a 'collback function'- a function that is applied to each element of the array(inside the function "map").
+//Have map return new array filled with numbers that are result of sing the 'callback function' on each of th input array.
+//map([1,2,3,4,5]), multiplyByTwo); //-> [2,4,6,8,10]
+//multiplyByTwo(1);  // ->2
+//multiplyByTwo(2);  // ->4
+function multiplyByTwo(num) {
+    return num*2
+}
+let value = [];
+function map(array, callback) {
+for(i=0; i<array.length; i++){
+    mul=multiplyByTwo(array[i]);
+    value.push(mul)
+
+}
+return value
+}
+console.log(map([1,2,3], multiplyByTwo))
